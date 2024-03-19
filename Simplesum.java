e72a7eae4d9120a0dd2089c8104b49975362f5a4
hasiyashaik <121428001+hasiyashaik@users.noreply.github.com>
2024-03-19 19:05:10 +0530

    Create Simplesum.java

---
+++ Simplesum.java
@@ -0,0 +1,23 @@
+public class SimpleSum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {100, 52, 43, 99, 42, 40, 91};
+
+        int sum = 0;
+
+        for (int number : numbers) {
+
+            if (number % 10 == 0) {
+
+                sum = sum + number;
+
+            }
+
+        }
+
+        System.out.println("The sum is: " + sum);
+
+    }
+
+}
